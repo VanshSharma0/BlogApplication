493b26f9947554ef7a960eb7e5342a9cefa0a8f0
VanshSharma0 <vanshsharma9933@gmail.com>
2024-08-30 18:53:09 +0530

    update footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,7 +4,7 @@ import Logo from '../logo'
 
 function Footer() {
   return (
-    <section className="relative overflow-hidden py-10 bg-gradient-to-r from-gray-800 via-gray-900 to-black border-t-2 border-t-black text-gray-300">
+    <section className="relative overflow-hidden py-10 bg-gradient-to-r from-gray-800 via-gray-900 to-black border-t-2 border-t-black text-gray-300 mt-auto">
       <div className="relative z-10 mx-auto max-w-7xl px-4">
         <div className="-m-6 flex flex-wrap">
           <div className="w-full p-6 md:w-1/2 lg:w-5/12">
@@ -14,7 +14,7 @@ function Footer() {
               </div>
               <div>
                 <p className="text-sm">
-                  &copy; Copyright 2023. All Rights Reserved by DevUI.
+                  &copy; Copyright 2024. All Rights Reserved by Vansh.
                 </p>
               </div>
             </div>
